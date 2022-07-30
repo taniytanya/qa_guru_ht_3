778daa90a0cd79727bcff7d0cb78e7f26e27e922
taniytanya <taniytanya@gmail.com>
2022-07-30 20:41:12 +0300

    change tests

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -5,21 +5,12 @@ import org.junit.jupiter.api.Test;
 
 public class FirstTest {
     @Test
-    void firstTest(){
-<<<<<<<<< Temporary merge branch 1
-        Assertions.assertFalse(13<1);
-    }
-    @Test
-    void secondTest(){
-        Assertions.assertFalse(13<2);
-=========
-
-        Assertions.assertFalse(3<2);
-    }
-    @Test
-    void secondTest(){
-        Assertions.assertFalse(4<=1);
->>>>>>>>> Temporary merge branch 2
+    void firstTest() {
+        Assertions.assertFalse(13 < 1);
     }
 
+    @Test
+    void secondTest() {
+        Assertions.assertFalse(13 < 2);
+    }
 }
