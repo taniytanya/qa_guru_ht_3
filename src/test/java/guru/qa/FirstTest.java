38f393f03b2728b11e1a14d3f6869f305290c654
taniytanya <taniytanya@gmail.com>
2022-07-30 20:19:34 +0300

    refactored test

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 public class FirstTest {
     @Test
     void firstTest(){
-        Assertions.assertTrue(3>2);
+        Assertions.assertFalse(3<2);
     }
     @Test
     void secondTest(){
-        Assertions.assertTrue(3>1);
+        Assertions.assertFalse(3<1);
     }
 }
